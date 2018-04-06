727784048a6493c21e983b227556089ed22e4384
Salma Tharwat <Salmatharwat51@gmail.com>
2018-04-06 12:03:44 +0200

    new functions
    player status
    print solved
    print unsolved

--- Sudoku/main.asm
+++ Sudoku/main.asm
@@ -13,12 +13,87 @@ value    byte ?
 row     dword  ?
 col     dword ?
 st1     byte  "Correct Answer",0
+WRONG_ANSWERS_COUNT byte 0
+CORRECT_ANSWERS_COUNT byte 0
 incrementation  byte ?
 r byte "Enter Row :" ,0
 c1 byte "Enter Colomn :" ,0
 wrongAns byte "Wrong Answer",0
+status1 byte "NUMBER OF WRONG ANSWERS",0
+status2 byte "NUMBER OF CORRECT ANSWERS",0
 .code
-;---------------------------------------------------------------------------------------
+;--------------------------------------PLAYERSTATUS------------------------------------------
+;Prints  the number of correct answers 
+;Prints number of Wrong Answers
+;prints time taken 
+;----------------------------------------------------------------------------------------
+PLAYER_STATUS  PROC
+mov al,WRONG_ANSWERS_COUNT
+mov edx,offset status1
+call writestring
+call crlf
+call writedec
+call crlf
+mov al,CORRECT_ANSWERS_COUNT
+mov edx,offset status2
+call writestring
+call crlf
+call writedec
+call crlf
+ret
+PLAYER_STATUS ENDP
+;--------------------------------------OPTION 2------------------------------------------
+;Prints  the unsolved  Grid -- Clear 
+;receives the offset of the unsolved grid
+;prints it out
+;----------------------------------------------------------------------------------------
+CLEAR_GRID  PROC
+mov esi,offset unsolved
+mov ecx,81
+mov ebx,0
+l1:
+     mov al,[esi]
+	 call writechar
+	 inc esi
+	 inc ebx
+	 cmp ebx,9
+	 jnz l2
+	 call crlf
+	 mov ebx,0
+	 loop l1
+	 jmp skip
+	 l2:
+	 loop l1
+	 skip:
+ret
+CLEAR_GRID ENDP
+;-------------------------------------OPTION 1-------------------------------------------
+;Prints Solved Grid (option 1)
+;receives the offset of the solved grid
+;prints it out
+;----------------------------------------------------------------------------------------
+PRINTSOLVED PROC
+mov esi,offset solved
+mov ecx,81
+mov ebx,0
+l1:
+     mov al,[esi]
+	 call writechar
+	 inc esi
+	 inc ebx
+	 cmp ebx,9
+	 jnz l2
+	 call crlf
+	 mov ebx,0
+	 loop l1
+	 jmp skip
+	 l2:
+	 loop l1
+	 skip:
+	 
+ret
+PRINTSOLVED ENDP
+;----------------------------------OPTION 3----------------------------------------------
 ;Checks the value enetered by the user wheather it fit the grid or no
 ;receives the value entered by the user,the row and colomn to place the value in 
 ;receives the offset of the solved suduko grid 
@@ -41,10 +116,20 @@ sub eax,'0'
 cmp value,al;; compare el fel ecx bel enetered value (law equal zero flag hykon b one
 jnz l1 ;; jump if xero flag=zero
 mov edx,offset st1 ;; move offset string okay
-call writestring ;; write okay 
+mov eax,2
+call SetTextColor
+call writestring ;; write correctanswer
+mov eax,15
+Call SetTextColor
+inc CORRECT_ANSWERS_COUNT
 RET
 l1: mov edx,offset wrongAns;; law msh zy ba3d hygy hena ( el mafrood cout wrong answer )
+    mov eax,4
+	call SetTextColor
     call writestring
+	mov eax,15
+Call SetTextColor
+inc WRONG_ANSWERs_COUNT
 RET
 check ENDP ;; end function
 main PROC
@@ -130,8 +215,20 @@ main PROC
    mov col,eax
    mov esi,offset solved ;;move offset of arrays
    mov edi ,offset unsolved
+   pushAD
    call check ;; proc call
-
+   call crlf
+   POPAD
+   PushAD
+   call PRINTSOLVED
+   POPAD
+   call crlf
+   pushAD
+   call CLEAR_GRID
+   popAD
+    pushAD
+   call PLAYER_STATUS
+   popAD
 	exit
 main ENDP
 
